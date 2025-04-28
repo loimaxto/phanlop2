d12ebbad73b60d0cc093c14614a8af30bf7ed39e
truong <truong263111@gmail.com>
2025-04-28 20:47:44 +0700

    chia nho cac file, demo fake api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,90 +1,74 @@
-import React from "react";
-import { NavLink } from "react-router";
+import React from 'react';
+import { NavLink } from 'react-router';
 export default function NavBar() {
-    return (
-        <aside
-            id="with-navbar-sidebar"
-            className="border-r-2 border-gray-500  overlay sm:shadow-none overlay-open:translate-x-0 drawer drawer-start hidden max-w-64 sm:absolute sm:z-0 sm:flex sm:translate-x-0 pt-16"
-            role="dialog"
-            tabindex="-1"
-        >
-            <div className="drawer-body  p-0 pt-4 ">
-                <ul className="menu">
-                    <li>
-                        <NavLink
-                            to="/ctdt"
-                            className={({ isActive }) =>
-                                isActive ? "bg-blue-200" : ""
-                            }
-                        >
-                            <span className="icon-[tabler--home] size-5"></span>
-                            Chương trình đào tạo
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/hoc-phan"
-                            className={({ isActive }) =>
-                                isActive ? "bg-blue-200" : ""
-                            }
-                        >
-                            <span className="icon-[tabler--home] size-5"></span>
-                            Học phần
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/de-cuong-chi-tiet"
-                            className={({ isActive }) =>
-                                isActive ? "bg-blue-200" : ""
-                            }
-                        >
-                            <span className="icon-[tabler--home] size-5"></span>
-                            Đề cương chi tiết
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/ke-hoach-day-hoc"
-                            className={({ isActive }) =>
-                                isActive ? "bg-blue-200" : ""
-                            }
-                        >
-                            <span className="icon-[tabler--home] size-5"></span>
-                            Kế hoạch dạy học
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/ke-hoach-giang-day"
-                            className={({ isActive }) =>
-                                isActive ? "bg-blue-200" : ""
-                            }
-                        >
-                            <span className="icon-[tabler--home] size-5"></span>
-                            Kế hoạch giảng dạy
-                        </NavLink>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span className="icon-[tabler--user] size-5"></span>
-                            User
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span className="icon-[tabler--login] size-5"></span>
-                            Sign In
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <span className="icon-[tabler--logout-2] size-5"></span>
-                            Sign Out
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </aside>
-    );
+  return (
+    <aside
+      id="with-navbar-sidebar"
+      className="border-r-2 border-gray-500  overlay sm:shadow-none overlay-open:translate-x-0 drawer drawer-start hidden max-w-64 sm:absolute sm:z-0 sm:flex sm:translate-x-0 pt-16"
+      role="dialog"
+      tabIndex="-1"
+    >
+      <div className="drawer-body  p-0 pt-4 ">
+        <ul className="menu">
+          <li>
+            <NavLink to="/ctdt" className={({ isActive }) => (isActive ? 'bg-blue-200' : '')}>
+              <span className="icon-[tabler--home] size-5"></span>
+              Chương trình đào tạo
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/hoc-phan" className={({ isActive }) => (isActive ? 'bg-blue-200' : '')}>
+              <span className="icon-[tabler--home] size-5"></span>
+              Học phần
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/de-cuong-chi-tiet"
+              className={({ isActive }) => (isActive ? 'bg-blue-200' : '')}
+            >
+              <span className="icon-[tabler--home] size-5"></span>
+              Đề cương chi tiết
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/ke-hoach-day-hoc/1"
+              className={({ isActive }) => (isActive ? 'bg-blue-200' : '')}
+            >
+              <span className="icon-[tabler--home] size-5"></span>
+              Kế hoạch dạy học demo
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/ke-hoach-giang-day"
+              className={({ isActive }) => (isActive ? 'bg-blue-200' : '')}
+            >
+              <span className="icon-[tabler--home] size-5"></span>
+              Kế hoạch giảng dạy
+            </NavLink>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon-[tabler--user] size-5"></span>
+              User
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon-[tabler--login] size-5"></span>
+              Sign In
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon-[tabler--logout-2] size-5"></span>
+              Sign Out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+  );
 }
--- src/pages/HocPhan/CreateHocPhanModal.jsx
+++ src/pages/HocPhan/CreateHocPhanModal.jsx
@@ -1,76 +1,98 @@
 export default function CreateHocPhanModal({ isOpen, onClose }) {
-    if (!isOpen) return null;
-    
-    return (
-        <div className="overlay bg-blend-screen modal opacity-100 duration-300" >
-            <div className="modal-dialog opacity-100 duration-300">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h3 className="modal-title">Tạo học phần</h3>
-                        <button onClick={onClose} type="button" className="btn btn-text btn-circle btn-sm absolute end-3 top-3"  >
-                            <span className="icon-[tabler--x] size-4"></span>
-                        </button>
-                    </div>
-                    <div className="modal-body items-center">
-                        <div class="w-full">
-                            <label class="label-text" for="input-ten-hp">Tên học phần</label>
-                            <input type="text" placeholder="John Doe" class="input" id="input-ten-hp" />
-                        </div>
-                        <div className="flex flex-row gap-4">
-                        <div class="max-w-32" >
-                                <label class="label-text" for="so-tinh-chi">Số tính chỉ:</label>
-                                <div class="input items-center">
-                                    <input class="text-center" type="number" min="0" id="so-tinh-chi" />
-                                </div>
+  if (!isOpen) return null;
 
-                            </div>
-                            <div class="max-w-32" >
-                                <label class="label-text" for="tiet-practical">Hệ số học phần:</label>
-                                <div class="input items-center">
-                                    <input class="text-center" type="number" min="0" id="tiet-practical" />
-                                </div>
-                            </div>
-                            </div>
-                        <div className="flex flex-row gap-4">
-                            
-                            <div class="max-w-32" >
-                                <label class="label-text" for="tiet-theory">Số tiết lý thuyết:</label>
-                                <div class="input items-center">
-                                    <input class="text-center" type="number" min="0" id="tiet-theory" />
-                                </div>
-                            </div>
-                            <div class="max-w-32" >
-                                <label class="label-text" for="tiet-practical">Số tiết thực hành:</label>
-                                <div class="input items-center">
-                                    <input class="text-center" type="number" min="0" id="tiet-practical" />
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <div class="w-full">
-                            <label class="label-text" for="select-nhom-kien-thuc"> Nhóm kiến thức</label>
-                            <select class="select" id="select-nhom-kien-thuc">
-                                <option>The Godfather</option>
-                            </select>
-                        </div>
-                        <div class="w-full">
-                            <label class="label-text" for="select-nhom-kien-thuc">Loại học phần</label>
-                            <select class="select" id="select-nhom-kien-thuc">
-                                <option value="1">Bắt buộc</option>
-                                <option value="2">Tùy chọn</option>
-                            </select>
-                        </div>
-                        <div class="w-full">
-                            <label class="label-text" for="input-hp-tien-quyet">Học phần tiên quyết</label>
-                            <input type="text" placeholder="fadsf" class="input" id="input-hp-tien-quyet" />
-                        </div>
-                    </div>
-                    <div className="modal-footer">
-                        <button onClick={onClose} type="button" className="btn btn-soft btn-secondary">Hủy</button>
-                        <button type="button" className="btn btn-primary">Xác nhận</button>
-                    </div>
-                </div>
+  return (
+    <div className="overlay bg-blend-screen modal opacity-100 duration-300">
+      <div className="modal-dialog opacity-100 duration-300">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h3 className="modal-title">Tạo học phần</h3>
+            <button
+              onClick={onClose}
+              type="button"
+              className="btn btn-text btn-circle btn-sm absolute end-3 top-3"
+            >
+              <span className="icon-[tabler--x] size-4"></span>
+            </button>
+          </div>
+          <div className="modal-body items-center">
+            <div className="w-full">
+              <label className="label-text" for="input-ten-hp">
+                Tên học phần
+              </label>
+              <input type="text" placeholder="John Doe" className="input" id="input-ten-hp" />
             </div>
+            <div className="flex flex-row gap-4">
+              <div className="max-w-32">
+                <label className="label-text" for="so-tinh-chi">
+                  Số tính chỉ:
+                </label>
+                <div className="input items-center">
+                  <input className="text-center" type="number" min="0" id="so-tinh-chi" />
+                </div>
+              </div>
+              <div className="max-w-32">
+                <label className="label-text" for="tiet-practical">
+                  Hệ số học phần:
+                </label>
+                <div className="input items-center">
+                  <input className="text-center" type="number" min="0" id="tiet-practical" />
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row gap-4">
+              <div className="max-w-32">
+                <label className="label-text" for="tiet-theory">
+                  Số tiết lý thuyết:
+                </label>
+                <div className="input items-center">
+                  <input className="text-center" type="number" min="0" id="tiet-theory" />
+                </div>
+              </div>
+              <div className="max-w-32">
+                <label className="label-text" for="tiet-practical">
+                  Số tiết thực hành:
+                </label>
+                <div className="input items-center">
+                  <input className="text-center" type="number" min="0" id="tiet-practical" />
+                </div>
+              </div>
+            </div>
+            <div className="w-full">
+              <label className="label-text" for="select-nhom-kien-thuc">
+                {' '}
+                Nhóm kiến thức
+              </label>
+              <select className="select" id="select-nhom-kien-thuc">
+                <option>The Godfather</option>
+              </select>
+            </div>
+            <div className="w-full">
+              <label className="label-text" for="select-nhom-kien-thuc">
+                Loại học phần
+              </label>
+              <select className="select" id="select-nhom-kien-thuc">
+                <option value="1">Bắt buộc</option>
+                <option value="2">Tùy chọn</option>
+              </select>
+            </div>
+            <div className="w-full">
+              <label className="label-text" for="input-hp-tien-quyet">
+                Học phần tiên quyết
+              </label>
+              <input type="text" placeholder="fadsf" className="input" id="input-hp-tien-quyet" />
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button onClick={onClose} type="button" className="btn btn-soft btn-secondary">
+              Hủy
+            </button>
+            <button type="button" className="btn btn-primary">
+              Xác nhận
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
